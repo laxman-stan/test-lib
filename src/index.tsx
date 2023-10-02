b352361315245b20aca4566425ab0dc5af5de70e
laxman-stan <laxmikant@getstan.app>
2023-10-02 19:20:54 +0530

    chore: syncoo

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+
+export function multiplySync(a: number, b: number): number {
+  return a * b;
+}
